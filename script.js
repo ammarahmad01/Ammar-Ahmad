3696ba24a6e4ddcecae52bdd0d9584172f992850
ammarahmad01 <140069776+ammarahmad01@users.noreply.github.com>
2024-08-09 13:02:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,26 @@
-let menuIcon = document.querySelector(' #menu- icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-let sections = document.querySelectorAll( 'section');
-let navlinks = document .querySelectorAll( 'header nav ul li a');
-
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
 
 window.onscroll = () => {
-sections. forEach(sec =>{
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-    
-    if(top >= offset & top < offset + height){
-        navLinks.forEach(links =>{
-            links.classlList.remove( 'active');
-                document .querySelector( 'header nav ul li a[href*=' + id + ' ]' ).classlist.add(active)
-        })       
-    }
-})
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
 }
 
-menuIcon.onclick= () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
-/*document.addEventListener("scroll", function() {
-    const sections = document.querySelectorAll("section");
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        if (sectionTop < window.innerHeight - 100) {
-            section.classList.add("show");
-        }
-    });
-});*/
